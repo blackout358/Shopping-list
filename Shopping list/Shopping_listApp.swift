e87526eff57345ceae8398acd764ff7ef9cc622f
blackout358 <bence.gellert3@gmail.com>
2023-05-19 09:17:49 -0700

    Initial Commit

---
+++ Shopping list/Shopping_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_listApp.swift
+//  Shopping list
+//
+//  Created by Bence Gеllért on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
