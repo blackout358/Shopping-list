b2069721d12a9b060a4f7930bd8de4e245f82ae3
blackout358 <bence.gellert3@gmail.com>
2023-06-06 01:55:15 -0700

    light mode dark mode toggle switch works with proper colours

--- Shopping list/ContentView.swift
+++ Shopping list/ContentView.swift
@@ -8,13 +8,24 @@ struct ContentView: View {
     @State var editMode = false
     @State var editText = ""
     @State var editID: UUID = UUID(uuidString: "E621E1F8-C36C-495A-93FC-0C247A3E6E5F")!
+    @State var darkMode:Bool = false
     @FocusState var isFocused: Bool
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.black
-                    .ignoresSafeArea()
+                if (darkMode) {
+                    Color.black
+                        .ignoresSafeArea()
+                        .preferredColorScheme(.dark)
+                }
+                else {
+                    Color.white
+                        .ignoresSafeArea()
+                        .preferredColorScheme(.light)
+                }
                 List {
+                    // if striked out is moved to none striked out, move item up and down function is broken
+                    // until the trash can is pressed to clear striked out items
                     ForEach(myItems.sorted(by: { !$0.isCompleted && $1.isCompleted}) , id: \.id) { item in
                         Button(action: {
                             if let index = myItems.firstIndex(where: { $0.id == item.id }) {
@@ -35,24 +46,17 @@ struct ContentView: View {
                                             isFocused.toggle()
                                         }
                                 }
-                                else {
-                                    Text("\(item.itemName)")
-                                        .strikethrough(item.isCompleted, color: .pink)
-                                }
                             }
                             else {
                                 Text("\(item.itemName)")
-                                    .strikethrough(item.isCompleted, color: .pink)
+                                    .strikethrough(item.isCompleted, color: .gray)
                             }
                         }
+                        .listRowBackground(darkMode ? nil : Color.gray .opacity(0.2))
                         .swipeActions(edge: .leading) {
                             Button(action: {
-//                                myItems.append(Items(itemName: "Edit Button Added This", isCompleted: false))
-//                                print(myItems)
-//                                writeJSON(items: myItems)
                                 editMode.toggle()
                                 isFocused.toggle()
-//                                print("Edit")
                                 print(item.id)
                                 if (editMode) {
                                     editID = item.id
@@ -68,9 +72,7 @@ struct ContentView: View {
                     .onDelete(perform: delete)
                     .onMove(perform: onMove)
                 }
-
                 .scrollContentBackground(.hidden)
-
             }
             .navigationBarTitle("Shopping list") // Set the title after the toolbar
             .navigationBarTitleDisplayMode(.large)
@@ -112,10 +114,14 @@ struct ContentView: View {
                     Button(action: {
                         myItems.removeAll(where: { $0.isCompleted})
                         writeJSON(items: myItems)
-                        
+
                     }) {
-                        Label("Refresh", systemImage: "trash")
+                        Label("Trash", systemImage: "trash")
                     }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Toggle("Test", isOn: $darkMode)
+                        .toggleStyle(.button)
                 }
                 ToolbarItem(placement: .keyboard) {
                     HStack{
